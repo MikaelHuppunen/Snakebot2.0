5ed2557dbd1b4b762b401f6f105f0babca94a9d7
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2023-10-23 03:30:24 +0300

    Optimized ai direction selection

--- Snakebot2.0/Snakebot2.0.cpp
+++ Snakebot2.0/Snakebot2.0.cpp
@@ -116,7 +116,6 @@ void selection() {
                 &bias[10 * i + 2 * ii + 1][0][0] + jjj + 1);
         }
     }
-    auto start = chrono::high_resolution_clock::now();
     for (int i = 1; i < gensize; i++) {
         //n mutations happens if chance of n mutations > (rand() / double(RAND_MAX)
         //Since RAND_MAX = 32767, if probability of n mutations is less than 1/32767, we ignore those possibilities
@@ -177,59 +176,62 @@ void selection() {
             }
         }
     }
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    timer2 += duration.count();
     copy(&bestmultiplier[0][0][0], &bestmultiplier[0][0][0] + 51290, &multiplier[0][0][0][0]);
     copy(&bestbias[0][0], &bestbias[0][0] + 100, &bias[0][0][0]);
 }
 
-void ai() {
+void ai(int attempt){
     for (int i = 0; i < score; i++) {
-        value[0][i] = snakex[i+1]/static_cast<float>(17);
-        value[0][255 + i] = snakey[i + 1] / static_cast<float>(15);
+        value[0][i] = snakex[i+1]/17.0;
+        value[0][255 + i] = snakey[i + 1] / 15.0;
     }
-    value[0][510] = x / static_cast<float>(17);
-    value[0][511] = y / static_cast<float>(15);
-    value[0][512] = foodx / static_cast<float>(17);
-    value[0][513] = foody / static_cast<float>(15);
+    value[0][510] = x / 17.0;
+    value[0][511] = y / 15.0;
+    value[0][512] = foodx / 17.0;
+    value[0][513] = foody / 15.0;
     value[0][514] = (d == 1);
     value[0][515] = (d == 2);
     value[0][516] = (d == 3);
     value[0][517] = (d == 4);
     for (int i = 0; i < 32; i++) {
-        value[1][i] = bias[att][0][i];
+        value[1][i] = bias[attempt][0][i];
         for (int ii = 0; ii < score; ii++) {
-            value[1][i] += value[0][ii] * multiplier[att][0][i][ii]
-                + value[0][ii+255] * multiplier[att][0][i][ii+255];
+            value[1][i] += value[0][ii] * multiplier[attempt][0][i][ii]
+                + value[0][ii+255] * multiplier[attempt][0][i][ii+255];
         }
         for (int ii = 510; ii < 518; ii++) {
-            value[1][i] += value[0][ii] * multiplier[att][0][i][ii];
+            value[1][i] += value[0][ii] * multiplier[attempt][0][i][ii];
         }
         value[1][i] *= (value[1][i] > 0);
     }
     for (int i = 0; i < 16; i++) {
-        value[2][i] = bias[att][1][i];
+        value[2][i] = bias[attempt][1][i];
         for (int ii = 0; ii < 32; ii++) {
-            value[2][i] += value[1][ii] * multiplier[att][1][i][ii];
+            value[2][i] += value[1][ii] * multiplier[attempt][1][i][ii];
         }
         value[2][i] *= (value[2][i] > 0);
     }
     for (int i = 0; i < 8; i++) {
-        value[3][i] = bias[att][2][i];
+        value[3][i] = bias[attempt][2][i];
         for (int ii = 0; ii < 16; ii++) {
-            value[3][i] += value[2][ii] * multiplier[att][2][i][ii];
+            value[3][i] += value[2][ii] * multiplier[attempt][2][i][ii];
         }
         value[3][i] *= (value[3][i] > 0);
     }
     for (int i = 0; i < 4; i++) {
-        value[4][i] = bias[att][3][i];
+        value[4][i] = bias[attempt][3][i];
         for (int ii = 0; ii < 8; ii++) {
-            value[4][i] += value[3][ii] * multiplier[att][3][i][ii];
+            value[4][i] += value[3][ii] * multiplier[attempt][3][i][ii];
         }
         value[4][i] *= (value[4][i] > 0);
     }
-    aid = distance(value[4], max_element(begin(value[4]), end(value[4]))) + 1;
+    int max_index = 0;
+    for(int i = 0; i < 4; i++) {
+        if(value[4][i] > value[4][max_index]) {
+            max_index = i;
+        }
+    }
+    aid = max_index + 1;
     if ((d - aid) % 2 != 0 || d == 0) {
         d = aid;
     }
@@ -324,7 +326,7 @@ int main(){
         for (int i = 0; i < tries; i++) {
             while(att < gensize){
                 timer++;
-                ai();
+                ai(att);
                 move();
             }
             att = 0;
