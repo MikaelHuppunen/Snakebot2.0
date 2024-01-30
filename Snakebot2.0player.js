cbb47b9eb714dd5053d3627ee3476752ce1fc069
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2024-01-30 22:16:06 +0200

    Improved code readability

--- Snakebot2.0player.js
+++ Snakebot2.0player.js
@@ -1,6 +1,6 @@
 let c = document.getElementById("s").getContext("2d"),j=32, X = 288,Y = 320, 
-score = 0,H = 0,s,b,d, m=Math.floor, gen = 0, att = 0, f, F, 
-crossoverpoint, timer = 0, totalfit, foodx = [], foody = [], dmin = 15, n = [{x:X,y:Y}], gensize = 200,
+score = 0,H = 0,s,b,d, att = 0,crossoverpoint, timer = 0, totalfit, foodx = []
+, foody = [], dmin = 15, n = [{x:X,y:Y}], gensize = 200,
 tries = 50, dstart, parentrank = [], deathkey = 0;
 let value = [];
 let multiplier = [];
@@ -49,19 +49,17 @@ for(let i = 0; i < 5; i++){
 
 function food(){
 	for(let i = 0; i < 260; i++){
-		foodx[i] = m(Math.random()*17+1) * j;
-		foody[i] = m(Math.random()*15+3) * j;
+		foodx[i] = Math.floor(Math.random()*17+1) * j;
+		foody[i] = Math.floor(Math.random()*15+3) * j;
 	}
 }
 food();
 
-f = foodx[0];
-F = foody[0];
-dstart = (Math.abs(X-f) + Math.abs(Y-F))/32;
+dstart = (Math.abs(X-foodx[0]) + Math.abs(Y-foody[0]))/32;
 dmin = dstart;
-while(Collision(f,F)){
-	f = m(Math.random()*17+1)*j;
-	F = m(Math.random()*15+3)*j;
+while(Collision(foodx[0],foody[0])){
+	foodx[0] = Math.floor(Math.random()*17+1)*j;
+	foody[0] = Math.floor(Math.random()*15+3)*j;
 }
 
 let output;
@@ -77,8 +75,8 @@ function ai(){
 	}
 	value[0][510] = ((X-32)/32)/17;
 	value[0][511] = ((Y-96)/32)/15;
-	value[0][512] = ((f-32)/32)/17;
-	value[0][513] = ((F-96)/32)/15;
+	value[0][512] = ((foodx[score]-32)/32)/17;
+	value[0][513] = ((foody[score]-96)/32)/15;
 	value[0][514] = (d==1);
 	value[0][515] = (d==2);
 	value[0][516] = (d==3);
@@ -150,49 +148,49 @@ function Collision(a,b){
     }
 }
 
-function e(a, b){
+function draw_circle(x, y){
 	c.beginPath();
-	c.arc(a + 16, b + 16, 16, 0, 7);
+	c.arc(x + 16, y + 16, 16, 0, 7);
 	c.fill()
 }
 
-function l(a){
-	c.fillStyle = a
+function set_fillstyle(fillstyle){
+	c.fillStyle = fillstyle;
 }
 
 
 function D(){
 	timer++;
 	ai();
-	l("#4a752c");
+	set_fillstyle("#4a752c");
 	c.fillRect(0, 0, 608, 72);
-	l("#578a34");
+	set_fillstyle("#578a34");
 	c.fillRect(0, 72, 608, 536);
-	l("#aad751");
+	set_fillstyle("#aad751");
 	c.fillRect(j, 96, 544, 480);
 	c.rect(j, 0, 576, 576);
 	c.clip();
 	for(let i = 999; i > 0; i--){
-		l("#a2d149");
-		c.fillRect((2*i-m(i/9)*18+m(i/9)%2)*j, m(i/9)*j+96, j, j)
+		set_fillstyle("#a2d149");
+		c.fillRect((2*i-Math.floor(i/9)*18+Math.floor(i/9)%2)*j, Math.floor(i/9)*j+96, j, j)
 	}
-	l("yellow");
-	e(484, 22);
+	set_fillstyle("yellow");
+	draw_circle(484, 22);
 	
-	l("black");
+	set_fillstyle("black");
 	c.font = "40px Arial";
     c.fillText(score,76,51);
     c.fillText(H,528,51);
     for(let i=0; i < n.length; i++){
-		l(i == 0? "green" : "white");
+		set_fillstyle(i == 0? "green" : "white");
 		c.fillRect(n[i].x,n[i].y,j,j);
 		c.strokeRect(n[i].x,n[i].y,j,j);
     }
 	
 	if(s>0&&s<5){d=s}
-	l("red");
-	e(j, 22);
-	e(f, F);
+	set_fillstyle("red");
+	draw_circle(j, 22);
+	draw_circle(foodx[score], foody[score]);
 	
     s = b*(d!=b-2&&d!=b+2);
 	
@@ -210,23 +208,21 @@ function D(){
 	if(d == 4){
 		Y+=j;
 	}
-    if(!(X<f||X>f||Y>F||Y<F)){
+    if(!(X<foodx[score]||X>foodx[score]||Y>foody[score]||Y<foody[score])){
         score++;
 		timer = 0;
-		f = foodx[score];
-		F = foody[score];
-		while(Collision(f,F) || (X == f && Y == F)){
-			f = m(Math.random()*17+1)*j;
-			F = m(Math.random()*15+3)*j;
+		while(Collision(foodx[score],foody[score]) || (X == foodx[score] && Y == foody[score])){
+			foodx[score] = Math.floor(Math.random()*17+1)*j;
+			foody[score] = Math.floor(Math.random()*15+3)*j;
 		}
-		dstart = (Math.abs(X-f) + Math.abs(Y-F))/32;
+		dstart = (Math.abs(X-foodx[score]) + Math.abs(Y-foody[score]))/32;
 		dmin = dstart;
     }else{
         n.pop();
     }
 	
-	if(dmin > (Math.abs(X-f) + Math.abs(Y-F))/32){
-		dmin = (Math.abs(X-f) + Math.abs(Y-F))/32;
+	if(dmin > (Math.abs(X-foodx[score]) + Math.abs(Y-foody[score]))/32){
+		dmin = (Math.abs(X-foodx[score]) + Math.abs(Y-foody[score]))/32;
 	}
     
     if(Collision(X,Y) || X < j || X > 544 || Y < 96 || Y > 544 || timer > 1000 || deathkey == 1){
@@ -246,13 +242,11 @@ function D(){
 			value[0][i] = 0;
 		}
 		food();
-		f = foodx[0];
-		F = foody[0];
-		while(Collision(f,F)){
-			f = m(Math.random()*17+1)*j;
-			F = m(Math.random()*15+3)*j;
+		while(Collision(foodx[0],foody[0])){
+			foodx[0] = Math.floor(Math.random()*17+1)*j;
+			foody[0] = Math.floor(Math.random()*15+3)*j;
 		}
-		dstart = (Math.abs(X-f) + Math.abs(Y-F))/32;
+		dstart = (Math.abs(X-foodx[0]) + Math.abs(Y-foody[0]))/32;
 		dmin = dstart;
     }
 	else{
